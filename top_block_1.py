4fa12d2ccee8c75905fc21853d9e9ffe6013b249
5uperhotman <42967101+5uperhotman@users.noreply.github.com>
2019-04-16 13:54:24 +0300

    Add files via upload

--- top_block_1.py
+++ top_block_1.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Tue Apr 16 13:44:35 2019
+# Generated: Tue Apr 16 13:37:40 2019
 ##################################################
 
 if __name__ == '__main__':
@@ -67,12 +67,12 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._slider_cutoff_range = Range(samp_rate/1000, samp_rate/2, 1, samp_rate/8, 200)
-        self._slider_cutoff_win = RangeWidget(self._slider_cutoff_range, self.set_slider_cutoff, "slider_cutoff", "counter_slider", float)
-        self.top_layout.addWidget(self._slider_cutoff_win)
         self._slider_trans_range = Range(samp_rate/1000, samp_rate/2, 1, samp_rate/8, 200)
         self._slider_trans_win = RangeWidget(self._slider_trans_range, self.set_slider_trans, "slider_trans", "counter_slider", float)
         self.top_layout.addWidget(self._slider_trans_win)
+        self._slider_cutoff_range = Range(samp_rate/1000, samp_rate/2, 1, samp_rate/8, 200)
+        self._slider_cutoff_win = RangeWidget(self._slider_cutoff_range, self.set_slider_cutoff, "slider_cutoff", "counter_slider", float)
+        self.top_layout.addWidget(self._slider_cutoff_win)
         self._slider_cutoff_high_range = Range(slider_cutoff, samp_rate/2, 1, slider_cutoff+2e03, 200)
         self._slider_cutoff_high_win = RangeWidget(self._slider_cutoff_high_range, self.set_slider_cutoff_high, "slider_cutoff_high", "counter_slider", float)
         self.top_layout.addWidget(self._slider_cutoff_high_win)
@@ -117,17 +117,17 @@ class top_block(gr.top_block, Qt.QWidget):
         
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, slider_cutoff, slider_trans, firdes.WIN_HAMMING, 6.76))
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.band_pass_filter_0 = filter.fir_filter_ccc(1, firdes.complex_band_pass(
-        	1, samp_rate, slider_cutoff, slider_cutoff_high, slider_trans, firdes.WIN_HAMMING, 6.76))
         self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.band_pass_filter_0, 0))    
-        self.connect((self.band_pass_filter_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.analog_noise_source_x_0, 0), (self.low_pass_filter_0, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_throttle_0, 0))    
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -143,7 +143,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.set_slider_cutoff(self.samp_rate/8)
         self.set_slider_trans(self.samp_rate/8)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.band_pass_filter_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, self.slider_cutoff, self.slider_cutoff_high, self.slider_trans, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.slider_cutoff, self.slider_trans, firdes.WIN_HAMMING, 6.76))
 
     def get_slider_cutoff(self):
         return self.slider_cutoff
@@ -151,21 +151,20 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_slider_cutoff(self, slider_cutoff):
         self.slider_cutoff = slider_cutoff
         self.set_slider_cutoff_high(self.slider_cutoff+2e03)
-        self.band_pass_filter_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, self.slider_cutoff, self.slider_cutoff_high, self.slider_trans, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.slider_cutoff, self.slider_trans, firdes.WIN_HAMMING, 6.76))
 
     def get_slider_trans(self):
         return self.slider_trans
 
     def set_slider_trans(self, slider_trans):
         self.slider_trans = slider_trans
-        self.band_pass_filter_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, self.slider_cutoff, self.slider_cutoff_high, self.slider_trans, firdes.WIN_HAMMING, 6.76))
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.slider_cutoff, self.slider_trans, firdes.WIN_HAMMING, 6.76))
 
     def get_slider_cutoff_high(self):
         return self.slider_cutoff_high
 
     def set_slider_cutoff_high(self, slider_cutoff_high):
         self.slider_cutoff_high = slider_cutoff_high
-        self.band_pass_filter_0.set_taps(firdes.complex_band_pass(1, self.samp_rate, self.slider_cutoff, self.slider_cutoff_high, self.slider_trans, firdes.WIN_HAMMING, 6.76))
 
 
 def main(top_block_cls=top_block, options=None):
